e6fb1171a11e90a0ab3d950c2544b234dde9e8d2
Ariel <ar.i117@hotmail.com>
2018-02-22 21:57:28 -0600

    Tags and random color for the player

--- Color Switch Replica/Assets/Player.cs
+++ Color Switch Replica/Assets/Player.cs
@@ -6,12 +6,44 @@ public class Player : MonoBehaviour {
     public float jumpForce = 10f;
     public Rigidbody2D rb;
 
+    public string currentColor;
+
+    private void Start()
+    {
+        SetRandomColor();    
+    }
 
     private void Update()
     {
         if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.tag);
+        //if (col.tag == "")
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0,3);
+        switch(index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                break;
+            case 1:
+                currentColor = "Yellow";
+                break;
+            case 2:
+                currentColor = "Magenta";
+                break;
+            case 3:
+                currentColor = "Pink";
+                break;
         }
     }
 }
--- Color Switch Replica/Assets/Rotator.cs
+++ Color Switch Replica/Assets/Rotator.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 100f;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
 	}
 }
